be6c4ebdf391f1be36759f43c5b5daf096995130
Davide Tello <tello.davide@gmail.com>
2021-03-31 15:44:00 +0200

    Add validations to Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/name'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
