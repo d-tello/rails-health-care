980c5922e028bcaa10d327309c0e69e0a4405da3
Davide Tello <tello.davide@gmail.com>
2021-04-01 11:46:29 +0200

    Send form works

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,12 +7,18 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def company_params
-    params.require(:company).permit(
-      name,
-      schedules_attributes: %i[id opens_at closes_at weekday closed _destroy]
-    )
+    params.require(:company).permit(:name, schedules_attributes: %i[id opens_at closes_at weekday closed _destroy])
   end
 end
