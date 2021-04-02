412a2d092676f0c15121d2c9a5af27b73a974433
Davide Tello <tello.davide@gmail.com>
2021-04-02 09:02:57 +0200

    Mad name unique while generating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@ def clear_database
 end
 
 def create_doctor
-  company = Company.create!(name: Faker::Artist.name)
+  company = Company.create!(name: Faker::Artist.unique.name)
   (1..7).each do |day|
     opening_time = Time.new(2021, 3, 31, (rand 8..10))
     closing_time = Time.new(2021, 3, 31, (rand 16..18))
-    schedule = Schedule.new(weekday: 1, opens_at: opening_time, closes_at: closing_time, closed: day >= 6)
+    schedule = Schedule.new(weekday: day, opens_at: opening_time, closes_at: closing_time, closed: day >= 6)
     schedule.company = company
     schedule.save
   end
